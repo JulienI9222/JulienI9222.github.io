3fdb60372328a30b81b1998217fca9c0ef6657a3
Julien Inera <JUlieni@nycstudents.net>
2025-02-12 21:30:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Julien Inera"; // update the part BETWEEN the "quotes"
-let userName = "JulienI9222"; // same here
+let ownerName = "Julien Inera"; // Julien Inera 
+let userName = "JulienI9222"; // JulienI9222
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
